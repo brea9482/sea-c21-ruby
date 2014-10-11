9ee2ac7374f51fa2a6a181b6db55f4b85ded0be3
Patrick Breault <breault.patrick@gmail.com>
2014-10-10 22:47:22 -0700

    Submit class2 assignment

--- lib/class2/exercise5.rb
+++ lib/class2/exercise5.rb
@@ -9,3 +9,11 @@
 #   What's your favorite number?
 #   42
 #   That's ok, I guess, but isn't 43 just a bit better?
+
+puts "What's your favorite number?"
+favorite_number = gets.chomp
+
+better_favorite_number = favorite_number.to_i + 1
+
+puts "That's ok, I guess, but isn't #{better_favorite_number}
+just a bit better?"
