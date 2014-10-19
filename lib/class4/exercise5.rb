3990b8d24a14fb8cd0a5bcfc68aa7143349f7888
Patrick Breault <breault.patrick@gmail.com>
2014-10-19 16:20:57 -0700

    Submit class4 assignment

--- lib/class4/exercise5.rb
+++ lib/class4/exercise5.rb
@@ -33,7 +33,78 @@
 
 # rubocop:disable MethodLength
 def modern_roman_numeral(num)
-  num # change me
+
+m = 0
+cm = 0
+d = 0
+cd = 0
+c = 0
+xc = 0
+l = 0
+xl = 0
+x = 0
+ix = 0
+v = 0
+iv = 0
+i = 0
+
+  if num == 1000
+    m = num/1000
+    num = num % 1000
+  end
+  if num >= 900
+    cm = num/900
+    num = num % 900
+  end
+  if num >= 500
+    d = num/500
+    num = num % 500
+  end
+  if num >= 400
+    cd = num/400
+    num = num % 400
+  end
+  if num >= 100
+    c = num/100
+    num = num % 100
+  end
+  if num >= 90
+    xc = num/90
+    num = num % 90
+  end
+  if num >= 50
+    l = num/50
+    num = num % 50
+  end
+    if num >= 40
+    xl = num/40
+    num = num % 40
+  end
+  if num >= 10
+    x = num/10
+    num = num % 10
+  end
+  if num >= 9
+    ix = num/9
+    num = num % 9
+  end
+  if num >= 5
+    v = num/5
+    num = num % 5
+  end
+  if num >= 4
+    iv = num/4
+    num = num % 4
+  end
+  if num >= 1
+    i = num/1
+  end
+
+
+print 'M'*m + 'CM'*cm + 'D'*d + 'CD'*cd + 'C'*c + 'XC'*xc + 'L'*l + 'XL'*xl +
+'X'*x + 'IX'*ix + 'V'*v + 'IV'*iv + 'I'*i
+
+
 end
 
 input = ARGV[0].to_i
--- scratch.rb
+++ scratch.rb
@@ -1,18 +1,94 @@
-# The secret of getting ahead is getting started.
-# - Mark Twain
+def roman number
 
+m_length = 0
+n900_length = 0
+d_length = 0
+n400_length = 0
+c_length = 0
+n90_length = 0
+l_length = 0
+n40_length = 0
+x_length = 0
+n9_length = 0
+v_length = 0
+n4_length = 0
+i_length = 0
 
-array = ['Can I Kick it?','A Tribe Called Quest','1991','Rump Shaker',\
-         'Wreckx-n-Effect','1992','Check Yo Self','Ice Cube','1993',\
-         'Regulate','Warren G & Nate Dogg','1994','I got 5 On It','Luniz',\
-         '1995','Ready Or Not','The Fugeed','1996']
+if number >= 1000
+m_length = number/1000
+number = number%1000
+end
 
+if number >= 900
+n900_length = number/900
+number = number%900
+end
 
-i = 0
-width = 50
+if number >= 500
+d_length = number/500
+number = number%500
+end
 
-while i < array.size
-puts array[i].ljust(width/2) + array[i + 1].center(width) + array[i + 2].rjust(width/2)
+if number >= 400
+n400_length = number/400
+number = number%400
+end
 
-i += 3
+if number >= 100
+c_length = number/100
+number = number%100
+end
+
+if number >= 90
+n90_length = number/90
+number = number%90
+end
+
+if number >= 50
+l_length = number/50
+number = number%50
+end
+
+if number >= 40
+n40_length = number/40
+number = number%40
+end
+
+if number >= 10
+x_length = number/10
+number = number%10
+end
+
+if number >= 9
+n9_length = number/9
+number = number%9
+end
+
+if number >= 5
+v_length = number/5
+number = number%5
+end
+
+if number >= 4
+n4_length = number/4
+number = number%4
+end
+
+if number < 4
+i_length = number/1
+number = number%10
+end
+
+puts 'M'*m_length + "CM"*n900_length + 'D'*d_length + 'CD'*n400_length + 'C'*c_length +
+'XC'*n90_length + 'L'*l_length + 'XL'*n40_length + 'X'*x_length + 'IX'*n9_length +
+'V'*v_length + 'IV'*n4_length + 'I'*i_length
+
+end
+
+x = ''
+while x != 'exit'
+puts 'Enter a number, and the equivalent roman numeral will be returned.'
+puts 'Type exit to quit.'
+x = gets.chomp
+roman x.to_i
 end
